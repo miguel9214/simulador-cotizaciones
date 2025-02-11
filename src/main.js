c31491d2b52933144ff0483159fcc87ca32088a8
miguel9214 <40032128+miguel9214@users.noreply.github.com>
2025-02-11 18:12:34 -0500

    fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 // Verifica si hay un idioma guardado en localStorage, si no, usa 'es'
 const idiomaGuardado = localStorage.getItem('idioma') || 'es';
