8852f93d6a63d28c3646f02314972bba1ea4dbbb
miguel9214 <40032128+miguel9214@users.noreply.github.com>
2025-02-11 14:37:38 -0500

    idioma

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Verifica si hay un idioma guardado en localStorage, si no, usa 'es'
+const idiomaGuardado = localStorage.getItem('idioma') || 'es';
+
+// Aplica el idioma al atributo lang del documento
+document.documentElement.lang = idiomaGuardado;
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
